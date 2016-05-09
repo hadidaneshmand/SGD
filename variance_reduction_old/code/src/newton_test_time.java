e1b0ec4c17b6b138442d227774820b299d7d6c7b
hadidaneshmand <hadi@hadi.inf.ethz.ch>
2016-05-09 11:32:45 +0200

    small change!!

--- variance_reduction_old/code/src/newton_test_time.java
+++ variance_reduction_old/code/src/newton_test_time.java
@@ -5,6 +5,7 @@ import opt.firstorder.First_Order_Factory_efficient;
 import opt.firstorder.LBFGS_my;
 import opt.firstorder.Newton;
 import opt.loss.Dyna_regularizer_loss_e;
+import opt.loss.Dyna_samplesize_loss_e;
 import opt.loss.Logistic_Loss_efficient;
 import opt.loss.Loss;
 import opt.loss.SecondOrderLoss;
@@ -22,18 +23,20 @@ public class newton_test_time {
 		int m = 100; 
 		loss.set_lambda(1.0/n);
 		DataPoint initParam = (DataPoint) DensePoint_efficient.one(Input.loss_train.getDimension()).multiply(3.0); 
-		FirstOrderOpt[] methods_in = new FirstOrderOpt[4]; 
+		FirstOrderOpt[] methods_in = new FirstOrderOpt[3]; 
 		SampleSizeStrategy strategy = new Adapt_Strategy_Double_Full(loss.getDataSize(), 3*d, 1, 1);
 		Dyna_regularizer_loss_e adapt_reg_loss = new Dyna_regularizer_loss_e(loss.clone_loss(), strategy.clone_strategy());
-		Dyna_regularizer_loss_e ssreg_loss_for_lbfg = new Dyna_regularizer_loss_e(loss.clone_loss(),strategy.clone_strategy()); 
+		Dyna_regularizer_loss_e ssreg_loss_for_lbfg = new Dyna_regularizer_loss_e(loss.clone_loss(),strategy.clone_strategy());
+		Dyna_samplesize_loss_e adapt_ss_loss = new Dyna_samplesize_loss_e(loss.clone_loss(), strategy);
+		methods_in[0] = new Newton((SecondOrderLoss) loss.clone_loss()); 
 		methods_in[1] = new Newton((SecondOrderLoss) loss.clone_loss()); 
-		methods_in[0] = new Newton((SecondOrderLoss) adapt_reg_loss);
-		methods_in[2] = new LBFGS_my(loss.clone_loss(), m); 
-		methods_in[3] = new LBFGS_my(ssreg_loss_for_lbfg, m); 
+		methods_in[2] = new Newton((SecondOrderLoss) adapt_reg_loss);
+//		methods_in[2] = new LBFGS_my(loss.clone_loss(), m); 
+//		methods_in[3] = new LBFGS_my(ssreg_loss_for_lbfg, m); 
 		for(int i=0;i<methods_in.length;i++){
 			methods_in[i].setParam(initParam);
 		}
 		First_Order_Factory_efficient.methods_in = methods_in; 
-		First_Order_Factory_efficient.experiment_with_iterations_complexity(1, loss.clone_loss(), 60, -1, Input.loss_test, Input.config.logDir+"_newton_time", Input.L, false,n);
+		First_Order_Factory_efficient.experiment_with_iterations_complexity(1, loss.clone_loss(), 60, -1, Input.loss_test, Input.config.logDir+"_newton_thomas", Input.L, false,n);
 	}
 }
