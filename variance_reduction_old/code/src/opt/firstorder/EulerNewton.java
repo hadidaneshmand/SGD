3b657383d96523343e244fc016e0b43ed605e3f0
hadidaneshmand <hadi@hadis-MacBook-Pro.local>
2016-05-11 22:42:54 +0200

    document for aurelien has been added

--- variance_reduction_old/code/src/opt/firstorder/EulerNewton.java
+++ variance_reduction_old/code/src/opt/firstorder/EulerNewton.java
@@ -8,10 +8,11 @@ import opt.loss.Loss;
 import opt.loss.SecondOrderLoss;
 
 
-public class EulerNewton extends FirstOrderOpt {
-	public EulerNewton(Loss loss) {
+public class EulerNewton extends Newton {
+	public EulerNewton(SecondOrderLoss loss) {
 		super(loss);
 		setStepSize(0.001);
+		loss.set_lambda(0.01);
 	}
 
 	@Override
@@ -21,25 +22,40 @@ public class EulerNewton extends FirstOrderOpt {
 
 	@Override
 	public void iterate_once() {
+		if(getLastLocalNorm()>0.00000001){ 
+			super.iterate_once() ;
+			return;
+		}
 		// Making Jacoobian Matrix 
 		SecondOrderLoss sloss = (SecondOrderLoss) loss; 
 		int d  = loss.getDimension(); 
 		Matrix hessian = sloss.getHessian(w);
-		Matrix wmat = new Matrix(d,1);
-		for(int i=0;i<d;i++){
-			wmat.set(i, 0, w.get(i));
-		}
+		System.out.println("stepsize:"+step_size);
 		Matrix A = new Matrix(d, d+1); 
 		A.setMatrix(0,d-1,0,d-1, hessian);
-		A.setMatrix(0,d-1,d,d, wmat);
-		Matrix AT = A.transpose(); 
+		System.out.println("set Matrix test:A:"+A.get(4, 3)+","+hessian.get(4, 3));
+		for(int i=0;i<d;i++){
+			A.set(i, d, w.get(i));
+		}
+		Matrix AT = new Matrix(d+1, d+1); 
+		AT.setMatrix(0, d, 0, d-1, A.transpose());
+		System.out.println("at:"+AT.get(2, d));
+		AT.set(d, d, loss.getLambda());
 		QRDecomposition QR = new QRDecomposition(AT); 
 		Matrix Q = QR.getQ(); 
-		Matrix R = QR.getR(); 
-		System.out.println("dimR:"+R.getRowDimension()+"*"+R.getColumnDimension());
-		System.out.println("dimQ:"+Q.getRowDimension()+"*"+Q.getColumnDimension());
+		Matrix R = QR.getR();
+		R = R.getMatrix(0, d-1, 0, d-1);
+//		System.out.println("R[2:0]:"+R.getMatrix(2, 2,0,d-1));
+//		System.out.println("dimR:"+R.getRowDimension()+"*"+R.getColumnDimension());
+//		System.out.println("dimQ:"+Q.getRowDimension()+"*"+Q.getColumnDimension());
 		double sigma = Math.signum(Q.det()*R.det()); 
+		//############## TEST ######################
+//		QRDecomposition QR_new = new QRDecomposition(A.transpose()); 
+//		Matrix R_new = QR_new.getR(); 
+//		System.out.println("|Q-Q'|:"+R_new.minus(R).normF());
+		//############## TEST ######################
 		System.out.println("sigma:"+sigma);
+		
 		// Jacoobian Moor Inverse 
 		Matrix Rinv = new Matrix(d+1, d); 
 		Rinv.setMatrix(0, d-1, 0, d-1, R.transpose().inverse());
@@ -59,6 +75,8 @@ public class EulerNewton extends FirstOrderOpt {
 		// compute matrix H
 		DataPoint w_new = mat2data(v, d); 
 		double lambda_new = v.get(d, 0); 
+		System.out.println("lambda by v update:"+lambda_new);
+		System.out.println("distance v to w:"+ w_new.squaredNormOfDifferenceTo(w));
 		loss.set_lambda(lambda_new);
 		DataPoint H_d = loss.getAverageGradient(w_new); 
 		Matrix H = new Matrix(d,1); 
@@ -68,6 +86,7 @@ public class EulerNewton extends FirstOrderOpt {
 		u = v.plus(A_inv.times(H).times(-1.0)); 
 		w = mat2data(u, d); 
 		loss.set_lambda(u.get(d, 0));
+		System.out.println("euler-loss:"+loss.computeLoss(w));
 	}
 	private DataPoint mat2data(Matrix in, int d){ 
 		DataPoint w_new = new DensePoint_efficient(d); 
@@ -80,7 +99,7 @@ public class EulerNewton extends FirstOrderOpt {
 
 	@Override
 	public FirstOrderOpt clone_method() {
-		EulerNewton en = new EulerNewton(loss); 
+		EulerNewton en = new EulerNewton((SecondOrderLoss) loss); 
 		en.setParam(this.clone_w());
 		en.setNum_computed_gradients(num_computed_gradients);
 		en.step_size = en.step_size; 
--- variance_reduction_old/docs/readme.txt
+++ variance_reduction_old/docs/readme.txt
@@ -1,7 +1,20 @@
-javac -d build/ -cp lib/Jama-1.0.3.jar:lib/EJML-core-0.26.jar:lib/jcommon-1.0.23.jar:lib/jfreechart-1.0.19.jar:lib/java-lsh-0.6.jar src/data/*.java src/plot/*.java src/opt/utils.java src/lsh/*.java src/stat/*.java src/opt/loss/*.java src/opt/*.java src/opt/config/*.java src/opt/externalcodes/*.java src/opt/firstorder/*.java src/opt/firstorder/old/*.java src/backup/*.java src/*.java
-java -cp .:build/ AdaptSAGA_Real
-bsub -R "rusage[mem=20480]" -W 48:30 -o "filename" < *.sh
-git add sth
-git commit -m "message"
-git push orgin master
-git pull 
+Compiling Info
+ javac -d build/ -cp lib/Jama-1.0.3.jar:lib/EJML-core-0.26.jar:lib/jcommon-1.0.23.jar:lib/jfreechart-1.0.19.jar:lib/java-lsh-0.6.jar src/data/*.java src/plot/*.java src/opt/utils.java src/lsh/*.java src/stat/*.java src/opt/loss/*.java src/opt/*.java src/opt/config/*.java src/opt/externalcodes/*.java src/opt/firstorder/*.java src/opt/firstorder/old/*.java src/backup/*.java src/*.java
+ java -cp .:build/ name_of_java_main_file address2config_file
+
+Runing on euler
+ bsub -R "rusage[mem=20480]" -W 48:30 -o "filename" < *.sh
+
+Git commands
+ git add sth
+ git commit -m "message"
+ git push orgin master
+ git pull 
+ 
+Details of packages: 
+opt: optimization package
++ opt.firstorder: all optimization methods
+++++ opt.firstorder.EulerNewton.java: implementation of Euler-Newton method to test run eulernewton_test.java 
++ opt.loss: all loss functions 
+
+
