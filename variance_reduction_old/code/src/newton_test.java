b7a37d4b39ec4ac1aaae3068060cbcabb8be83b2
hadidaneshmand <hadi@hadi.inf.ethz.ch>
2016-05-16 15:41:46 +0200

    a bug fixed

--- variance_reduction_old/code/src/newton_test.java
+++ variance_reduction_old/code/src/newton_test.java
@@ -39,6 +39,6 @@ public class newton_test{
 //			methods_in[i].setParam(initParam);
 //		}
 		First_Order_Factory_efficient.methods_in = methods_in; 
-		First_Order_Factory_efficient.experiment_with_iterations_complexity(3, loss.clone_loss(), 100, 0, Input.loss_test, Input.config.logDir+"_newton_datadriven", Input.L, false,n);
+		First_Order_Factory_efficient.experiment_with_iterations_complexity(3, loss.clone_loss(), 100, -1.0, Input.loss_test, Input.config.logDir+"_newton_datadriven", Input.L, false,n);
 	}
 }
