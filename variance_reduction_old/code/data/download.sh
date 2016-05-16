04ba29624fddf215aaf262ad114324a0c0f1b69b
Hadi Daneshmand <dhadi@euler04.hpc-lca.ethz.ch>
2016-05-16 18:33:20 +0200

    data/download.sh

--- variance_reduction_old/code/data/download.sh
+++ variance_reduction_old/code/data/download.sh
@@ -1,3 +1,11 @@
 wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/w8a
 wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/SUSY.bz2
 bzip2 -d SUSY.bz2
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a9a
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/ijcnn1.bz2
+bzip2 -d ijcnn1.bz2
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/covtype.libsvm.binary.bz2
+bzip2 -d covtype.libsvm.binary.bz2
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/rcv1_train.binary.bz2
+bzip2 -d rcv1_train.binary.bz2
+
