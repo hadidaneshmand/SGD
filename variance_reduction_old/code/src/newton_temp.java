61fcb69a44efa8d343e8cb64f33038b39c58a8a5
hadidaneshmand <hadi@hadi.inf.ethz.ch>
2016-05-17 15:56:36 +0200

    small changes

--- variance_reduction_old/code/src/newton_temp.java
+++ variance_reduction_old/code/src/newton_temp.java
@@ -1,15 +1,8 @@
-import data.DataPoint;
-import data.DensePoint_efficient;
 import opt.Adapt_Strategy_Double_Full;
 import opt.SampleSizeStrategy;
 import opt.firstorder.FirstOrderOpt;
 import opt.firstorder.First_Order_Factory_efficient;
-import opt.firstorder.LBFGS_external;
-import opt.firstorder.LBFGS_my;
-import opt.firstorder.Newton;
 import opt.firstorder.NewtonDataDriven;
-import opt.firstorder.SAGA;
-import opt.loss.Dyna_regularizer_loss_e;
 import opt.loss.Loss;
 import opt.loss.SecondOrderLoss;
 
@@ -20,17 +13,14 @@ public class newton_temp{
 //		Logistic_Loss_efficient.buildHessians(Input.data, Input.d);
 		int n = loss.getDataSize();
 		int d = loss.getDimension(); 
-		int m = 100; 
 		loss.set_lambda(1.0/n);
 		FirstOrderOpt[] methods_in = new FirstOrderOpt[1]; 
 		SampleSizeStrategy strategy = new Adapt_Strategy_Double_Full(loss.getDataSize(), 3*d, 1, 1);
-		Dyna_regularizer_loss_e adapt_reg_loss = new Dyna_regularizer_loss_e(loss.clone_loss(), strategy.clone_strategy());
-		Dyna_regularizer_loss_e ssreg_loss_for_lbfg = new Dyna_regularizer_loss_e(loss.clone_loss(),new Adapt_Strategy_Double_Full(loss.getDataSize(), 3*d, 3, 12)); 
-		methods_in[0] = new NewtonDataDriven((SecondOrderLoss) loss.clone_loss(),strategy,2.0);
+		methods_in[0] = new NewtonDataDriven((SecondOrderLoss) loss.clone_loss(),strategy,1.0);
 //		for(int i=0;i<methods_in.length;i++){
 //			methods_in[i].setParam(initParam);
 //		}
 		First_Order_Factory_efficient.methods_in = methods_in; 
-		First_Order_Factory_efficient.experiment_with_iterations_complexity(3, loss.clone_loss(), 100, -1.0 , Input.loss_test, Input.config.logDir+"_newton_datadriven", Input.L, false,n);
+		First_Order_Factory_efficient.experiment_with_iterations_complexity(3, loss.clone_loss(), 100,0 , Input.loss_test, Input.config.logDir+"_newton_datadriven", Input.L, false,n);
 	}
 }
